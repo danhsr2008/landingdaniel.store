77f1a9ec2bdae44cb87d34d75cef9f270ea10633
Daniel Ribeiro <danhsr2008@gmail.com>
2025-05-14 20:34:13 -0300

    .

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  safelist: ['shiny-text'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
